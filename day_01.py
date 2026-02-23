01fd347c9ae2e634dfc67396604531f5697f6123
AnkurAnand404 <ankurofficial0005@gmail.com>
2026-02-23 11:36:28 +0530

    day-01 introduction to python

---
+++ day_01.py
@@ -0,0 +1,101 @@
+print("Hello world")
+
+x = 20
+y = 30
+
+print(x) #20
+print(y) #30
+
+#comment
+
+""" doc sting 
+multiline comment 
+and doumenting some features.
+
+"""
+
+a, b = 40, 50
+
+y = 90
+z = 90
+print(y) #90
+print(a,b)
+
+print(f"value of x is {x} and value of y is {y}") #string interpolation
+
+print(id(x))
+print(id(y))
+print(id(90))
+print(id(30))
+
+
+#Operators
+
+print( x + y) # 110
+print( x - y) 
+print( x* y)
+print( y/x)
+print( y // x)
+b = 3
+v = 4
+print( b ** v) 
+
+# user input
+#x = input("enter any value x")
+
+
+print(x)
+#y = input("enter any value y")
+print(y)
+
+print(x + y)
+#print(type(x))
+#print(type(y))
+
+#type casting
+
+# 1. implicit type cast
+a = 10
+b = 20.8
+print(a + b) # 30.8 
+
+#2. Explicit type cast
+x = int(input("enter any number x"))
+y = int(input("enter value y"))
+print(x + y)
+
+
+a = 10
+b = a 
+a = a + 1
+
+print(b) #10
+print(a) #11
+
+print(id(a))
+print(id(b))
+
+#data type 
+#1 numeric 
+# int 
+# float 
+price = 40.90
+tax = 50.9
+
+# bool
+islogged = True # 1
+isloggedout = False # 0
+
+print(islogged)
+print(isloggedout)
+
+# complex number
+e = 6 + 5j
+
+# str
+a = "ankur"
+
+
+
+
+
